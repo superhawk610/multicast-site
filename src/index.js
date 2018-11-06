4784c65f08930a2bc4712d51b6cad03072e64b41
Aaron Ross <superhawk610@gmail.com>
2018-11-05 23:41:40 -0500

    fix text/hero sizing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import greenHeart from './images/green-heart.png';
 const renderTarget = document.getElementById('readme');
 const renderMarkdown = md.replace(
   ':green_heart:',
-  `<img src="${greenHeart}" style="height: 12px" />`
+  `<img src="${greenHeart}" style="height: 15px" />`
 );
 renderTarget.innerHTML = renderMarkdown;
