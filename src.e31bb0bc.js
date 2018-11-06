df0a34d6c02391fea85fb1e7184796db221af33d
Aaron Ross <superhawk610@gmail.com>
2018-11-05 23:47:12 -0500

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41687" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34279" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
